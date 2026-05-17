c6a74386d073caf5f2b971a75a5c44ea69d2a271
Sagar <spadhiyar230595@gmail.com>
2026-05-17 15:34:32 +0530

    demo added

---
+++ 1.LLMs/1_llm_demo.py
@@ -0,0 +1,12 @@
+# pyrefly: ignore [missing-import]
+from langchain_openai import OpenAI
+# pyrefly: ignore [missing-import]
+from dotenv import load_dotenv
+
+load_dotenv()
+
+llm = OpenAI(model='gpt-3.5-turbo-instruct')
+
+result = llm.invoke("What is the capital of India")
+
+print(result)
