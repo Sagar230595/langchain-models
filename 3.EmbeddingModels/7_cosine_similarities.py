994eda029d9e2aa1c48a187901c3b417acc99b82
Sagar <spadhiyar230595@gmail.com>
2026-06-13 19:52:49 +0530

    cosine similarity code added

---
+++ 3.EmbeddingModels/7_cosine_similarities.py
@@ -0,0 +1,36 @@
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+from dotenv import load_dotenv
+from sklearn.metrics.pairwise import cosine_similarity
+import numpy as np
+
+load_dotenv()
+
+embeddings_model = GoogleGenerativeAIEmbeddings(
+    model="models/gemini-embedding-2",
+    output_dimensionality=32
+)
+
+documents = [
+    "Virat Kohli is an Indian cricketer known for his aggressive batting and leadership.",
+    "MS Dhoni is a former Indian captain famous for his calm demeanor and finishing skills.",
+    "Sachin Tendulkar, also known as the 'God of Cricket', holds many batting records.",
+    "Rohit Sharma is known for his elegant batting and record-breaking double centuries.",
+    "Jasprit Bumrah is an Indian fast bowler known for his unorthodox action and yorkers."
+]
+
+query = 'tell me about MS Dhoni'
+
+document_embeddings = [
+    embeddings_model.embed_documents([doc])[0] 
+    for doc in documents
+]
+
+query_embedding = embeddings_model.embed_query(query)
+
+scores = cosine_similarity([query_embedding], document_embeddings)[0]
+
+index, score = sorted(list(enumerate(scores)),key=lambda x:x[1])[-1]
+
+print(query)
+print(documents[index])
+print("similarity score is:", score)
